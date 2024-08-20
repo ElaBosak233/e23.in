76f81becb679e56db79976d5ba1a0e41a09ca756
埃拉 <ElaBosak233@qq.com>
2024-08-20 17:11:53 +0800

    wip: unocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import UnoCSS from "unocss/astro";
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind(),
-		icon({
-			mdi: ["github"]
-		})
-	],
+    integrations: [
+        UnoCSS({ injectReset: true }),
+        icon({
+            mdi: ["github"],
+        }),
+    ],
 });
